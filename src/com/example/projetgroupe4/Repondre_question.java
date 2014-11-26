ddc3d4d56902e00bf7322e46c7e629424ca5d5be
romaindemoustier <romain.demoustier@condorcet.be>
2014-11-26 12:53:56 +0100

    test_Menu

--- src/com/example/projetgroupe4/Repondre_question.java
+++ src/com/example/projetgroupe4/Repondre_question.java
@@ -3,11 +3,11 @@ package com.example.projetgroupe4;
 import Modele.QuestionsDB;
 import Modele.UtilisateurDB;
 import MyConnection.DBConnection;
-import android.support.v7.app.ActionBarActivity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -23,7 +23,7 @@ public class Repondre_question extends ActionBarActivity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.Repondre_question, menu);
+		getMenuInflater().inflate(R.menu.repondre_question, menu);
 		return true;
 	}
 
