f21dad7302bee5b20e964a08559647e1434a9d54
User1 <varctor@gmail.com>
2014-11-26 10:43:52 +0100

    TU COMPRENDS CA !!!

---
+++ src/com/example/projetgroupe4/Rentrer_id.java
@@ -0,0 +1,149 @@
+package com.example.projetgroupe4;
+
+import java.sql.Connection;
+
+import Modele.QuestionsDB;
+import Modele.UtilisateurDB;
+import MyConnection.DBConnection;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Rentrer_id extends ActionBarActivity {
+	
+	private Button valider=null;
+	private Button deco=null;
+    private TextView bad_id=null;
+    private Connection con=null;
+    private EditText ed1;
+	 private EditText ed2;
+	
+	QuestionsDB questions = new QuestionsDB();
+	UtilisateurDB utilisateur = new UtilisateurDB();
+	
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.rentrer_id);
+		
+		valider=(Button)findViewById(R.id.valid_id_q);
+		deco=(Button)findViewById(R.id.deco_id);
+		ed1=(EditText) findViewById(R.id.texte1);
+		ed2=(EditText) findViewById(R.id.texte2);
+
+		//bad_id=(TextView)findViewById(R.id.id
+		
+		
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.rentrer_id, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	
+	 class MyAccesDB extends AsyncTask<String,Integer,Boolean> {
+		    private String resultat;
+		    private ProgressDialog pgd=null;
+
+
+					public MyAccesDB(MainActivity pActivity) {
+
+						link(pActivity);
+						// TODO Auto-generated constructor stub
+					}
+
+					private void link(MainActivity pActivity) {
+						// TODO Auto-generated method stub
+
+
+					}
+
+					protected void onPreExecute(){
+						 super.onPreExecute();
+				         pgd=new ProgressDialog(Rentrer_id.this);
+						 pgd.setMessage("chargement en cours");
+						 pgd.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+			     		 pgd.show();
+
+					}
+
+					@Override
+					protected Boolean doInBackground(String... arg0) {
+
+					   if(con==null){//premier invocation
+						   con = new DBConnection().getConnection();
+					    	if(con==null) {
+						    resultat="echec de la connexion";
+					      	return false;
+						    }
+
+						   QuestionsDB.setConnection(con);
+						   UtilisateurDB.setConnection(con);
+					   }
+					    int id=Integer.parseInt(ed1.getText().toString());
+				        try{
+
+				           QuestionsDB cl=new QuestionsDB(id);
+				           cl.read();
+				           resultat=cl.toString();
+
+				        }
+				        catch(Exception e){
+				         resultat="erreur" +e.getMessage();
+				         return false;
+
+				         }
+
+
+						return true;
+					}
+
+					protected void onPostExecute(Boolean result
+							{
+						 super.onPostExecute(result);
+						 if (result)
+						 {
+							 
+							 Toast.makeText(Connect.this,"Connexion à votre compte...",Toast.LENGTH_SHORT).show();
+							 Intent i = new Intent(Connect.this,Repondre_question.class);	
+			                 i.putExtra(ID_USER, utilisateur); 
+							 startActivity(i);
+							 finish();
+					     } 
+					     else 
+					     {
+					    	 mdp.setText("");
+					    	 Toast.makeText(Connect.this,resultat,Toast.LENGTH_SHORT).show();
+					     }
+						
+						 
+					}
+
+				}
+}
